f487d19058e05fe93082cac6b357be7ccff96a76
Fabio <fabizen99@gmail.com>
2026-06-05 10:16:50 +0200

    FIX: removed some slop name

--- CTFd/plugins/challenge_discussion/assets/js/discussion.js
+++ CTFd/plugins/challenge_discussion/assets/js/discussion.js
@@ -1,7 +1,8 @@
 /**
  * challenge_discussion/assets/js/discussion.js
  *
- * Injects two buttons into the challenge modal footer:
+ * Injects two buttons into the challenge modal footer that navigate
+ * in the same tab:
  *   - "General"  → /challenges/{id}/discuss/general  (open to all)
  *   - "Spoiler"  → /challenges/{id}/discuss/spoiler  (solve-gated)
  */
@@ -32,15 +33,13 @@
             var a = document.createElement("a");
             a.href = url;
             a.className = "btn btn-sm discussion-plugin-btn " + cls;
-            a.target = "_blank";
-            a.rel = "noopener noreferrer";
             a.style.marginLeft = "6px";
             a.innerHTML = '<i class="fas ' + icon + '"></i> ' + label;
             return a;
         }
 
-        footer.appendChild(makeBtn("General",  "fa-comments", "btn-outline-info",    "/challenges/" + challengeId + "/discuss/general"));
-        footer.appendChild(makeBtn("Spoiler",  "fa-lock",     "btn-outline-warning", "/challenges/" + challengeId + "/discuss/spoiler"));
+        footer.appendChild(makeBtn("General discussion",  "fa-comments", "btn-outline-info",    "/challenges/" + challengeId + "/discuss/general"));
+        footer.appendChild(makeBtn("Post solution discussion",  "fa-lock",     "btn-outline-warning", "/challenges/" + challengeId + "/discuss/spoiler"));
     }
 
     document.addEventListener("shown.bs.modal", function (e) {
